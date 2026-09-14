8f93dcd544e5d40956943be5340be35c70ebc6ab
Röbbe Wünschiers <rw@rwaire.local>
2026-09-14 15:43:46 +0200

    faa

--- .sandboxbio/autoblast.sh
+++ .sandboxbio/autoblast.sh
@@ -4,5 +4,5 @@
 for i in 1 0.001 0.00001
 do
 echo "Working on h7vsk12-$i.txt"
-blastp -db ecolik12 -query ec-h7.faa -out h7vsk12-$i.txt -evalue $i
+blastp -db ecolik12 -query ec-h7.fasta -out h7vsk12-$i.txt -evalue $i
 done
